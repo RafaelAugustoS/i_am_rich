f95988cbe9e9197ced5ff370d436fa753a00bce0
Rafael Augusto <rafaelaugusto.developer@gmail.com>
2019-09-24 16:48:30 -0300

    create AppBar and Body with Image

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
   MaterialApp(
-    home: Center(
-      child: Text('Hello World')
-    )
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text('I Am Rich'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: NetworkImage('http://br.web.img3.acsta.net/r_640_360/newsv7/19/09/23/20/37/4533833.jpg')
+        )
+      )
+    ),
   )
 );
